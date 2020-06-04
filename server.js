31ab1989be62f0f6cd0771d5620cebd400292b85
Shashi Singh <shasingh36@publicisgroupe.net>
2020-06-04 12:22:57 +0530

    server file modified

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.static(__dirname + '/build'));
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { 
     res.sendFile(path.resolve(__dirname, "build", "index.html"));
 })
 
